db2319d1e107d7fc805761fe30f0c8cb8c0ee245
Shubham Kumar <mr.dev.shubham@gmail.com>
2024-06-25 10:08:50 +0530

    store class add

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
